3847dcc7a366574b254452ec16a3724ce662909e
NachoMol <ignaciotmolina@gmail.com>
2023-10-24 20:50:00 -0300

    colocando titulo y descripcion. Modificando ubicacón body

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -5,19 +5,23 @@ const Detail = () => {
   return (
     <>
     <header className='detail_header'>
-      <h2 className='detail_title'>Titulo del producto</h2>
+      <h2 className='detail_title'>Chevrolet Cruze II 1.4 LTZ 153CV Hatchback 2016</h2>
       <Link to={'/'}>
       <img className='back-logo' src="../../public/images/back-logo.png" alt="volver al inicio" />
       </Link>
     </header>
+    <body>
     <div>
     <Link to={'/CarGallery'}>
     <button>More photos</button>
     </Link>
     </div>
     <div>
-    <body>texto descriptivo del producto e imagenes</body>
+      <p>Año 2016</p>
+      <p>Version: 1.4 Ltz 153cv</p>
+      <p>Transmisión: Manual</p>
     </div>
+    </body>
     </>
   )
 }
