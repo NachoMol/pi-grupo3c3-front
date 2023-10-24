ece8fdb0e3b142d71a30728795d4c369799a35ca
NachoMol <ignaciotmolina@gmail.com>
2023-10-23 23:28:17 -0300

    fixeando padding del root y agregando color al header del detail

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Detail = () => {
   return (
     <>
     <header className='detail_header'>
       <h2>Titulo del producto</h2>
-      <p>Volver atras</p>
+      <Link to={'/'}>
+        <p>Volver al inicio</p>
+      </Link>
     </header>
     <body>texto descriptivo del producto e imagenes</body>
     </>
