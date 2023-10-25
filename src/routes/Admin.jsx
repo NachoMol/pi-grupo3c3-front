365ae8a5b0c71c183985ae3ce8b992ca43b7550f
NachoMol <ignaciotmolina@gmail.com>
2023-10-25 19:49:41 -0300

    agregando panel de Admin con registrar producto (no funcional)

---
+++ src/routes/Admin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <>
+    <body>
+        <button className='action-button'>Registrar Producto</button>
+    </body>
+    </>
+  )
+}
+
+export default Admin
