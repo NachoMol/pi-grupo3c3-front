a1f36a35b82b3be7b921328fc8dfce08a9d48b0e
FlorenciaCasal <florenciacasal88.fc@gmail.com>
2023-10-26 09:27:28 -0300

    Últimos detalles responsive, en ruta Detail

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import CarGallery from '../components/CarGallery'
+import '../styless/App.css'
 import '../styless/Detail.css'
 
 const Detail = () => {
