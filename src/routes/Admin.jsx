9b3957445da683ac6ad6a77afac8acf6c7adfdef
ramiromp <ramiro.marpreuss@gmail.com>
2023-10-26 15:30:36 -0300

    Formulario corregido estilos basicos

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -27,11 +27,14 @@ const Admin = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    // Verificar si el nombre ya existe en la lista de vehículos
     if (vehiclesList.some((v) => v.name === vehicle.name)) {
       setError('The name is already in use.');
     } else {
       setError('');
       setVehiclesList([...vehiclesList, vehicle]);
+
+      // Reiniciar el formulario
       setVehicle({
         name: '',
         brand: '',
@@ -47,62 +50,68 @@ const Admin = () => {
     <div>
       <h2>Register Vehicle</h2>
       <form onSubmit={handleSubmit}>
-        <label>Name:</label>
-        <input
-          type="text"
-          name="name"
-          value={vehicle.name}
-          onChange={handleInputChange}
-          required
-        />
+      <label className="label">Name:</label>
+<input
+  className="input-text"
+  type="text"
+  name="name"
+  value={vehicle.name}
+  onChange={handleInputChange}
+  required
+/>
 
-        <label>Brand:</label>
-        <input
-          type="text"
-          name="brand"
-          value={vehicle.brand}
-          onChange={handleInputChange}
-          required
-        />
+<label className="label">Brand:</label>
+<input
+  className="input-text"
+  type="text"
+  name="brand"
+  value={vehicle.brand}
+  onChange={handleInputChange}
+  required
+/>
 
-        <label>Images (One URL per line):</label>
-        <textarea
-          name="images"
-          value={vehicle.images.join('\n')}
-          onChange={handleImageChange}
-          required
-        />
+<label className="label">Images (One URL per line):</label>
+<textarea
+  className="textarea"
+  name="images"
+  value={vehicle.images.join('\n')}
+  onChange={handleImageChange}
+  required
+/>
 
-        <label>Vehicle Type:</label>
-        <input
-          type="text"
-          name="type"
-          value={vehicle.type}
-          onChange={handleInputChange}
-          required
-        />
+<label className="label">Vehicle Type:</label>
+<input
+  className="input-text"
+  type="text"
+  name="type"
+  value={vehicle.type}
+  onChange={handleInputChange}
+  required
+/>
 
-        <label>Number of Seats:</label>
-        <input
-          type="number"
-          name="seats"
-          value={vehicle.seats}
-          onChange={handleInputChange}
-          required
-        />
+<label className="label">Number of Seats:</label>
+<input
+  className="input-number"
+  type="number"
+  name="seats"
+  value={vehicle.seats}
+  onChange={handleInputChange}
+  required
+/>
 
-        <label>Price:</label>
-        <input
-          type="number"
-          name="price"
-          value={vehicle.price}
-          onChange={handleInputChange}
-          required
-        />
+<label className="label">Price:</label>
+<input
+  className="input-number"
+  type="number"
+  name="price"
+  value={vehicle.price}
+  onChange={handleInputChange}
+  required
+/>
 
-        {error && <p className="error-message">{error}</p>}
+{error && <p className="error-message">{error}</p>}
 
-        <button type="submit">Submit</button>
+<button className="button" type="submit">Submit</button>
       </form>
     </div>
   );
