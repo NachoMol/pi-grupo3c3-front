fd3f8607661dfbb2bffe1ebcaea84b97497d55c3
NachoMol <ignaciotmolina@gmail.com>
2023-10-27 00:00:54 -0300

    dejando archivo context limpio

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -3,28 +3,4 @@ import api from '../axiosConfig';
 
 const ProductContext = createContext();
 
-export function useProduct() {
-  return useContext(ProductContext);
-}
-
-export function ProductProvider({ children }) {
-    const [products, setProducts] = useState([]);
-  
-    const addProduct = async (newProduct) => {
-      try {
-        const response = await api.post('/cars', newProduct);
-        setProducts([...products, response.data]);
-      } catch (error) {
-        console.error('Error while trying to register a car:', error);
-      }
-    };
-
-    return (
-        <ProductContext.Provider value={{ products, addProduct }}>
-          {children}
-        </ProductContext.Provider>
-      );
-    }
-
-
 
