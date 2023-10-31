668e6fee58beb5523ca718513186c2d0a9f86b56
NachoMol <ignaciotmolina@gmail.com>
2023-10-31 20:44:58 -0300

    fixeando color header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -52,7 +52,7 @@ const Header = () => {
   };
 
   return (
-    <AppBar position="sticky" sx={{ backgroundColor: '#371957' }}>
+    <AppBar position="sticky" sx={{ backgroundColor: '#5C4D6B' }}>
       <Container maxWidth={false} >
         <Toolbar disableGutters sx={{ maxHeight: 90 }}>
           <Box disableGutters sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, maxWidth: 135 }}>
