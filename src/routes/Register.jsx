4cdd542f20e3915d208a91a1e13f69c4d866f49c
NachoMol <ignaciotmolina@gmail.com>
2023-11-01 21:06:33 -0300

    eliminando checkbox de notificacion

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -91,12 +91,6 @@ return (
                   autoComplete="new-password"
                 />
               </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
             </Grid>
             <Button
               type="submit"
