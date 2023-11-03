545a13a6aade47723eb8594a23d935cf214c2e6f
NachoMol <ignaciotmolina@gmail.com>
2023-11-02 21:55:41 -0300

    Implementando mui en details

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,39 +1,72 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import CarGallery from '../components/CarGallery'
-import '../styless/App.css'
-import '../styless/Detail.css'
+import { Button, CssBaseline, Grid, Typography } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles'; // Importa ThemeProvider y createTheme
+import {Container} from '@mui/material' 
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CarGallery from '../components/CarGallery';
+import '../styless/App.css';
+import '../styless/Detail.css';
+
+const theme = createTheme(); // Configura el tema de Material-UI
 
 const Detail = () => {
+  const carDetails = {
+    price: '$20,000',
+    categoryId: 'Sedan',
+    location: 'Los Angeles, CA',
+    specs: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula...',
+  };
+
   return (
-    <>
-    <header className='detail_header'>
-      <Link to={'/'} className='back-logo-container'>
-      <img className='back-logo' src="https://www.iconpacks.net/icons/3/free-icon-left-arrow-7252.png" alt="" />
-      </Link>
-      <h2 className='detail_title'>Chevrolet Cruze II 1.4 LTZ 153CV Hatchback 2016</h2>
-    </header>
-    
-    <div className='detail-div'>
-    <div>
-      <CarGallery/>
-    {/* <Link to={'/CarGallery'}>
-    <button>More photos</button>
-    </Link> */}
-    </div>
-    <div className='detail-information'>
-      <h3>Car details</h3>
-      <p><strong>Brand: </strong>Chevrolet</p>
-      <p><strong>Model: </strong>Hatchback 2016</p>
-      <p><strong>Car Type: </strong>Basic</p>
-      <p><strong>Seats: </strong>4</p>
-      <h4><strong>Price: </strong>$10.0000 per day</h4>
-      <button className='rent-button'>Rent now</button>
-    </div>
-    </div>
-    
-    </>
-  )
+    <ThemeProvider theme={theme}> {/* Proporciona el tema de Material-UI */}
+      <header className='detail_header'>
+        <Link to={'/'} className='back-logo-container'>
+          <img className='back-logo' src="https://www.iconpacks.net/icons/3/free-icon-left-arrow-7252.png" alt="" />
+        </Link>
+        <h2 className='detail_title'>Chevrolet Cruze II 1.4 LTZ 153CV Hatchback 2016</h2>
+      </header>
+
+      <div className='detail-div'>
+        <div>
+          <CarGallery />
+        </div>
+        <div className='detail-information'>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Typography component="h1" variant="h5">
+              Car Details
+            </Typography>
+
+            {/* Detalles del coche */}
+            <Typography variant="subtitle1">
+              Price: {carDetails.price}
+            </Typography>
+            <Typography variant="subtitle1">
+              Category: {carDetails.categoryId}
+            </Typography>
+            <Typography variant="subtitle1">
+              Location: {carDetails.location}
+            </Typography>
+
+            {/* Sección de detalles */}
+            <Typography variant="h6" style={{ marginTop: '20px' }}>
+              Specifications
+            </Typography>
+            <Typography variant="body1">
+              {carDetails.specs}
+            </Typography>
+
+            {/* Botón de reserva */}
+            <Grid container justifyContent="center" style={{ marginTop: '20px' }}>
+              <Button variant="contained" onClick={() => alert('Make a Reservation')} sx={{ mt: 3, mb: 2, bgcolor: '#302253', '&:hover': { bgcolor: '#5e2b96' } }}>
+                Make a Reservation
+              </Button>
+            </Grid>
+          </Container>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default Detail
+export default Detail;
