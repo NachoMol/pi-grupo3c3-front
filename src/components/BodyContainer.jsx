23da9125bcd5095074216a08f652d80a9c7e1d3a
NachoMol <ignaciotmolina@gmail.com>
2023-11-03 18:26:03 -0300

    renderizando productos random en el home

--- src/components/BodyContainer.jsx
+++ src/components/BodyContainer.jsx
@@ -11,123 +11,14 @@ import { useCarStates } from '../context/Context';
 import CategoryList from '../routes/CategoryList';
 import axios from 'axios';
 
-//const car = [
-//     {
-//         id: 1,
-//         marca: "Peugeot",
-//         modelo: "3008",
-//         año: 2020,
-//         imagenURL: "https://images.coches.com/_vo_/fotos/usados/2023/10/23/c/24405926360767122-767122_1.JPG?p=cc_vo_high",
-//         precio: '$20.500.000'
-
-//     },
-//     {
-//         id: 2,
-//         marca: "Volkswagen",
-//         modelo: "Polo 1.6",
-//         año: 2020,
-//         imagenURL: "https://http2.mlstatic.com/D_NQ_NP_2X_763126-MLA72005410208_102023-F.webp",
-//         precio: '$24.000.000'
-//     },
-//     {
-//         id: 3,
-//         marca: "Toyota",
-//         modelo: "Corolla",
-//         año: 2020,
-//         imagenURL: "https://http2.mlstatic.com/D_NQ_NP_2X_763602-MLA72486253515_102023-F.webp",
-//         precio: '$26.000.000'
-//     },
-//     {
-//         id: 4,
-//         marca: "Nissan",
-//         modelo: "Kicks 1.6",
-//         año: 2021,
-//         imagenURL: "https://http2.mlstatic.com/D_NQ_NP_2X_827752-MLA71519788842_092023-F.webp",
-//         precio: '$38.000.000'
-//     },
-//     {
-//         id: 5,
-//         marca: "Toyota",
-//         modelo: "Corolla",
-//         año: 2022,
-//         imagenURL: "https://http2.mlstatic.com/D_NQ_NP_2X_626203-MLA72266204706_102023-F.webp",
-//         precio: '$49.000.000'
-//     },
-//     {
-//         id: 6,
-//         marca: "Toyota",
-//         modelo: "Hilux 2.8 Srx",
-//         año: 2021,
-//         imagenURL: "https://http2.mlstatic.com/D_NQ_NP_2X_861217-MLA71523331207_092023-F.webp",
-//         precio: '$40.000.000'
-//     },
-//     {
-//         id: 7,
-//         marca: "Toyota",
-//         modelo: "Hilux 2.8 Srv",
-//         año: 2021,
-//         imagenURL: "https://http2.mlstatic.com/D_NQ_NP_2X_886436-MLA71782239281_092023-F.webp",
-//         precio: '$37.000.000'
-//     },
-//     {
-//         id: 8,
-//         marca: "Nissan",
-//         modelo: "Note 1.6 Sense",
-//         año: 2019,
-//         imagenURL: "https://http2.mlstatic.com/D_NQ_NP_2X_800784-MLA72529678829_102023-F.webp",
-//         precio: '$29.000.000'
-//     },
-//     {
-//         id: 9,
-//         marca: "Chevrolet",
-//         modelo: "Onix 1.0 turbo",
-//         año: 2021,
-//         imagenURL: "https://http2.mlstatic.com/D_809852-MLA72478813147_102023-O.jpg",
-//         precio: '$32.000.000'
-//     },
-//     {
-//         id: 10,
-//         marca: "Chevrolet",
-//         modelo: "Onix 1.4",
-//         año: 2021,
-//         imagenURL: "https://http2.mlstatic.com/D_NQ_NP_2X_829487-MLA72160004323_102023-F.webp",
-//         precio: '$50.000.000'
-//     },
-//     {
-//         id: 11,
-//         marca: "Chevrolet",
-//         modelo: "Spin",
-//         año: 2020,
-//         imagenURL: "https://http2.mlstatic.com/D_NQ_NP_960326-MLA71908103486_092023-W.webp",
-//         precio: '$22.000.000'
-//     },
-//     {
-//         id: 12,
-//         marca: "Renault",
-//         modelo: "Stepway",
-//         año: 2021,
-//         imagenURL: "https://http2.mlstatic.com/D_NQ_NP_2X_920544-MLA72365752073_102023-F.webp",
-//         precio: '$24.000.000'
-//     },
-//     {
-//         id: 13,
-//         marca: "Renault",
-//         modelo: "Stepway",
-//         año: 2022,
-//         imagenURL: "https://http2.mlstatic.com/D_NQ_NP_2X_849940-MLA72104822759_102023-F.webp",
-//         precio: '$30.000.000'
-//     }
-// ];
-
 const BodyContainer = () => {
 
     const[cars, setCars] = useState([]);
-    const [randomArray, setRandomArray] = useState([]);
 
     useEffect(() => {
         const fetchCars = async () => {
         try{
-            const response = await axios.get('http://localhost:8080/products')
+            const response = await axios.get('http://localhost:8080/products/random')
             setCars(response.data)
         }catch(error){
             console.error("Error fetching cars", error)
@@ -139,14 +30,7 @@ const BodyContainer = () => {
 
     },[])
 
-    
-
-    useEffect(() => {
-        setRandomArray(shuffleArray(cars));
-    }, [])
-
     console.log('initialArray', cars);
-    console.table('randomArray', randomArray);
 
     return (
         <Container disableGutters maxWidth='1980px' sx={{ background: '#D9D9D9;' }} >
@@ -170,7 +54,7 @@ const BodyContainer = () => {
             />
             <CategoryList />
             <Grid container spacing={2} sx={{ mt: '1rem' }}>
-                {randomArray.map((cars, index) => (
+                {cars.map((cars, index) => (
                     <Grid item key={index} xs={12} sm={12} md={6} lg={6} xl={6} sx={{ display: 'Flex', justifyContent: 'center', paddingRight: '16px' }}>
                         <Link to={'/detail/'} style={{ textDecoration: 'none' }}>
                             <Card sx={{ maxWidth: 350, background: 'transparent', minWidth: 349 }}>
