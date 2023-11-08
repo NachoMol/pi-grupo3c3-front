675b5377ba8c713d0eb7627cfbb235e28810be37
NachoMol <ignaciotmolina@gmail.com>
2023-11-07 21:25:43 -0300

    colocando fixeada la category a nivel logica de seleccion

--- src/routes/InsertVehicle.jsx
+++ src/routes/InsertVehicle.jsx
@@ -15,9 +15,9 @@ const InsertVehicle = () => {
   const [vehicle, setVehicle] = useState({
     name: '',
     price: '',
-    category: '', // Cambiado para almacenar la categoría seleccionada como un string
+    category: { id: '' }, // Cambiado para almacenar la categoría como un objeto
     location: '',
-    details: [], // Aquí almacenaremos los detalles seleccionados
+    details: [],
   });
 
   const [categories, setCategories] = useState([]);
@@ -47,10 +47,26 @@ const InsertVehicle = () => {
     setVehicle({ ...vehicle, [name]: value });
   };
 
+
+  
   const handleCategoryChange = (event) => {
-    const { value } = event.target;
-    setVehicle({ ...vehicle, category: value });
+    const categoryId = event.target.value; // Obten el ID de la categoría seleccionada
+  
+    // Encuentra la categoría correspondiente en la lista de categorías
+    
+  
+    if (categoryId) {
+      // Si se encuentra una categoría correspondiente, establece el objeto de categoría en el estado
+      setVehicle({ ...vehicle, category: { id: categoryId } }); // Establece el ID de la categoría en un objeto
+      console.log('Categoría válida:', categoryId);
+    } else {
+      // Si el ID no es válido, puedes manejarlo aquí (por ejemplo, mostrar un mensaje de error)
+      console.log('Categoría no válida. Valor no válido:', categoryId);
+    }
+  
+    console.log('Selected Category ID:', categoryId);
   };
+
 
   const handleDetailsChange = (event) => {
     const { name, checked } = event.target;
@@ -78,33 +94,38 @@ const InsertVehicle = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+  
     try {
+      // Obtén el ID de la categoría seleccionada
+      const selectedCategoryId = vehicle.category.id;
+  
       // Enviar la solicitud para crear el producto
       const productResponse = await axios.post('http://localhost:8080/products/create', {
-        ...vehicle,
-        details: selectedDetails, // Usar los detalles seleccionados
+        name: vehicle.name,
+        price: vehicle.price,
+        category: { id: selectedCategoryId }, // Envía el ID de la categoría
+        details: selectedDetails,
       });
-
+  
       const vehicleID = productResponse.data.id;
-
+  
       console.log('Selected Details:', selectedDetails);
-
+  
       // Limpia los detalles seleccionados y otros campos
       setSelectedDetails([]);
       setImages([]);
-
+  
       const formData = new FormData();
       for (let i = 0; i < images.length; i++) {
         formData.append('file', images[i]);
       }
-
+  
       await axios.post('http://localhost:8080/media/upload', formData);
-
+  
       setVehicle({
         name: '',
         price: '',
-        category: '',
+        category: '', // También puedes eliminar esto ya que no se usa
         location: '',
         details: [],
       });
@@ -148,8 +169,8 @@ const InsertVehicle = () => {
                 {categories.map((category) => (
                   <FormControlLabel
                     key={category.id}
-                    value={category.id}
-                    control={<Radio />}
+                    value={category.id}  // Asegúrate de que esto coincida con el valor de la categoría
+                    control={<Radio checked={vehicle.category === category.id} />} // Verifica si es igual al valor de la categoría actual
                     label={category.name}
                   />
                 ))}
