c71b50675c9868893728f17395cfd5421c46d613
ramiromp <ramiro.marpreuss@gmail.com>
2023-11-16 19:41:53 -0300

    UserProfile del avatar terminado

--- src/routes/UserProfile.jsx
+++ src/routes/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useContextGlobal } from '../context/Context';  // Asegúrate de que la ruta del contexto sea correcta
+import { useContextGlobal } from '../context/Context';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
@@ -7,33 +7,62 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { styled } from '@mui/system';
 
-const StyledTableCell = TableCell;  // No es necesario modificar el estilo
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+    fontWeight: 'bold',
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+    textAlign: 'center',
+  },
+}));
 
-const StyledTableRow = TableRow;  // No es necesario modificar el estilo
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+}));
 
 const UserProfile = () => {
   const { userData } = useContextGlobal();
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+    <TableContainer component={Paper} sx={{ mt: 3, backgroundColor: '#f5f5f5', width: '90vw', marginLeft: 'auto', marginRight: 'auto', overflowX: 'auto' }}>
+      <Table sx={{}} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <StyledTableCell>User Information</StyledTableCell>
-            <StyledTableCell align="right">Name</StyledTableCell>
-            <StyledTableCell align="right">Last Name</StyledTableCell>
-            <StyledTableCell align="right">Email</StyledTableCell>
+            <StyledTableCell align="center">Attribute</StyledTableCell>
+            <StyledTableCell align="center">Value</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           <StyledTableRow>
             <StyledTableCell component="th" scope="row">
-              {userData.user.email ? userData.user.email : 'No email available'}
+              User Id
             </StyledTableCell>
-            <StyledTableCell align="right">{userData.user.name}</StyledTableCell>
-            <StyledTableCell align="right">{userData.user.lastname}</StyledTableCell>
-            <StyledTableCell align="right">{userData.user.email}</StyledTableCell>
+            <StyledTableCell align="center">{userData.user.id}</StyledTableCell>
+          </StyledTableRow>
+          <StyledTableRow>
+            <StyledTableCell component="th" scope="row">
+              Name
+            </StyledTableCell>
+            <StyledTableCell align="center">{userData.user.name}</StyledTableCell>
+          </StyledTableRow>
+          <StyledTableRow>
+            <StyledTableCell component="th" scope="row">
+              Last Name
+            </StyledTableCell>
+            <StyledTableCell align="center">{userData.user.lastname}</StyledTableCell>
+          </StyledTableRow>
+          <StyledTableRow>
+            <StyledTableCell component="th" scope="row">
+              Email
+            </StyledTableCell>
+            <StyledTableCell align="center">{userData.user.email}</StyledTableCell>
           </StyledTableRow>
         </TableBody>
       </Table>
