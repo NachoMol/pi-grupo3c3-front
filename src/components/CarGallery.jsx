2a730c6a735a9aea68958669739c1c7d8a2e04e2
ramiromp <ramiro.marpreuss@gmail.com>
2023-12-01 19:20:24 -0300

    pequenos detalles del detail del auto (imagenes)

--- src/components/CarGallery.jsx
+++ src/components/CarGallery.jsx
@@ -4,23 +4,8 @@ import PropTypes from 'prop-types';
 import { fetchImageUrl } from '../utils/fetchImageUrl';
 
 const CarGallery = ({productImages = [], productId}) => {
-  const [isZoomed, setIsZoomed] = useState(false);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [productImageUrls, setProductImageUrls] = useState([]);
-
-  const toggleZoom = () => {
-    setIsZoomed(!isZoomed);
-  };
-
-  const handleZoomPreviousClick = () => {
-    const newIndex = (currentImageIndex - 1 + productImages.length) % productImages.length;
-    setCurrentImageIndex(newIndex);
-  };
-
-  const handleZoomNextClick = () => {
-    const newIndex = (currentImageIndex + 1) % productImages.length;
-    setCurrentImageIndex(newIndex);
-  };
 
   useEffect(() => {
     const fetchImageUrls = async () => {
@@ -45,52 +30,41 @@ const CarGallery = ({productImages = [], productId}) => {
     fetchImageUrls();
   }, [productImages, productId, fetchImageUrl]);
   
-
   // Verificación condicional para manejar el caso cuando productImages es undefined o vacío
   if (!productImages || productImages.length === 0) {
     return <div>No images available</div>;
   }
 
   return (
-    <div className={`gallery ${isZoomed ? 'zoomed' : ''}`}>
+    <div className="gallery">
       <div className='mainPhotoArrowContainer'>
-        <div className="arrow left-arrow" onClick={handleZoomPreviousClick}>
-          <img className={`arrowIcons ${isZoomed ? 'visible' : ''}`} src="https://www.iconpacks.net/icons/2/free-icon-arrow-left-3099.png" alt="" />
+        <div className="arrow left-arrow" onClick={() => setCurrentImageIndex(prevIndex => (prevIndex - 1 + productImages.length) % productImages.length)}>
+          <img className="arrowIcons" src="https://www.iconpacks.net/icons/2/free-icon-arrow-left-3099.png" alt="" />
         </div>
         <img
-          className={`mainPhoto ${isZoomed ? 'zoomed-photo' : ''}`}
+          className="mainPhoto"
           src={productImageUrls[currentImageIndex]}
           alt=''
-          onClick={!isZoomed ? toggleZoom : null}
         />
-        <div className="arrow right-arrow" onClick={handleZoomNextClick}>
-          <img className={`arrowIcons ${isZoomed ? 'visible' : ''}`} src="https://www.iconpacks.net/icons/2/free-icon-arrow-right-3098.png" alt="" />
+        <div className="arrow right-arrow" onClick={() => setCurrentImageIndex(prevIndex => (prevIndex + 1) % productImages.length)}>
+          <img className="arrowIcons" src="https://www.iconpacks.net/icons/2/free-icon-arrow-right-3098.png" alt="" />
         </div>
-        {isZoomed && (
-          <div className="close-zoom" onClick={toggleZoom}>
-            <img className='zoomCross' src="https://icon-library.com/images/white-cross-icon/white-cross-icon-3.jpg" alt="" />
-          </div>
-        )}
       </div>
-      <ul className={`photosContainer ${isZoomed ? 'zoomed-photos-container' : ''}`}>
+      <ul className="photosContainer">
         {productImageUrls.map((imageUrl, index) => (
-          <li key={index} className={`photos ${isZoomed ? 'zoomed-photos' : ''}`}>
+          <li key={index} className="photos">
             <img
-              className={`photo ${isZoomed ? 'zoomed-secondary-photos' : ''}`}
+              className="photo"
               src={imageUrl}
               alt=''
-              onClick={() => {
-                if (!isZoomed) {
-                  setCurrentImageIndex(index);
-                }
-              }}
+              onClick={() => setCurrentImageIndex(index)}
             />
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 CarGallery.propTypes = {
   productImages: PropTypes.arrayOf(PropTypes.string).isRequired,
