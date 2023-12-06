77e3a5716f2e8f2005e7c4b820567c768cec558d
FlorenciaCasal <florenciacasal88.fc@gmail.com>
2023-12-06 15:35:20 -0300

    más estilos ReservationPage

--- src/routes/ReservationPage.jsx
+++ src/routes/ReservationPage.jsx
@@ -3,13 +3,15 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { TextField, Box, Divider, Grid, Container, Button } from '@mui/material';
+import { TextField, Box, Paper, Grid, Typography, Container, Button } from '@mui/material';
 import { useContextGlobal, useCarStates } from '../context/Context';
 import ProductPolicies from './ProductPolicies';
 import CarGallery from '../components/CarGallery';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import '../styless/ReservationPage.css';
+import '../styless/Detail.css';
+import '../styless/CarGallery.css';
 
 const ReservationPage = ({ selectedDates }) => {
     const params = useParams();
@@ -50,57 +52,63 @@ const ReservationPage = ({ selectedDates }) => {
 
     const confirmReservation = async () => {
         const reservationDTO = {
-          product_id: product.id,
-          user_id: userData.user.id,
-          city_id: product.city.id,
-          checkin: selectedDates.startDate, // Use the selected start date
-          checkout: selectedDates.endDate, // Use the selected end date
+            product_id: product.id,
+            user_id: userData.user.id,
+            city_id: product.city.id,
+            checkin: selectedDates.startDate, // Use the selected start date
+            checkout: selectedDates.endDate, // Use the selected end date
         };
-      
+
         try {
-          const response = await axios.post('http://localhost:8080/reservations/create', reservationDTO);
-          console.log('Reserva creada:', response.data);
+            const response = await axios.post('http://localhost:8080/reservations/create', reservationDTO);
+            console.log('Reserva creada:', response.data);
         } catch (error) {
-          console.error('Error:', error);
+            console.error('Error:', error);
         }
-      };
+    };
 
     return (
         <Container maxWidth="lg">
             <Grid container spacing={2}>
                 <Grid item xs={12}>
-                <h2 style={{ textAlign: 'center', paddingBottom:'10px' }}>Reservation Details</h2>
+                    <h2 style={{ textAlign: 'center', paddingBottom: '10px' }}>Reservation Details</h2>
                 </Grid>
-                <Grid container spacing={2} sx={{display: 'flex', justifyContent:"space-between", alignItems:'start', margin:'0px'}}>
+                <Grid container spacing={2} sx={{ display: 'flex', justifyContent: "space-between", alignItems: 'start', margin: '0px', width: '100%' }}>
                     {/* Imagenes */}
-                    <Grid item xs={12} sm={6} sx={{paddingTop:'0px !important', paddingRight: '60px' }}>
-                        <CarGallery productImages={product.images} productId={product.id}/>
+                    <Grid item xs={12} sm={6} sx={{ paddingTop: '0px !important', paddingRight: '60px' }}>
+                        <CarGallery productImages={product.images} productId={product.id} />
                     </Grid>
-                {/* Calendario */}
-                
-                <div className="Calendario" style={{ padding: '0px 10px', border: '1px solid #aeaeae', backgroundColor: '#aeaeae', width: '45%'}}>
+                    {/* Calendario */}
+
+                    <div className="Calendario" style={{ padding: '0px 10px', border: '1px solid #aeaeae', backgroundColor: '#aeaeae', width: '45%', minHeight: '400px' }}>
                         <h3 style={{ backgroundColor: 'rgb(156,128,189)', padding: '10px', borderRadius: '5px' }}>Checkin/Checkout</h3>
-                    <DatePicker
-                        selectsRange
-                        inline
-                        monthsShown={window.innerWidth > 1261 ? 2 : 2}
-                        //highlightDates={highlightDates}
-                        style={{ width: '50%', backgroundColor: 'transparent' }}
-                    />
-                    <Grid container justifyContent="center" style={{ width: '100%' }}>
+                        <DatePicker
+                            selectsRange
+                            inline
+                            monthsShown={window.innerWidth > 1261 ? 2 : 2}
+                            //highlightDates={highlightDates}
+                            style={{ width: '50%', backgroundColor: 'transparent' }}
+                        />
+                        {/* <Grid container justifyContent="center" style={{ width: '100%' }}>
                         <Button variant="contained" onClick={confirmReservation} sx={{ mt: 1, mb: 2, bgcolor: '#302253', '&:hover': { bgcolor: '#5e2b96' } }}>
                             Confirm Reservation                    
                         </Button>
-                    </Grid>
-                </div>
+                    </Grid> */}
+                    </div>
 
-                
-                    
+
+
                 </Grid>
 
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
+                {/* <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}> */}
+                {/*Formulario*/}
+                {/* <form>*/}
+
+                <div style={{ display: 'flex', flexDirection: 'column', marginTop: '100px', borderRadius: '5px', justifyContent: 'center', alignItems: 'center', height: 'auto', backgroundColor: '#d9d9d9', width: '100%' }}>
+                    <h2>Reservation Verification</h2>
                     {/*Formulario*/}
-                    <form>
+                    <form style={{ backgroundColor: '#fff', borderRadius: '5px', padding: '10px', boxShadow: '0px 0px 10px rgba(0,0,0,0.15)' }}>
+
                         <Grid container spacing={2} justifyContent="center">
                             <Grid item xs={12} sm={6} md={3}>
                                 <TextField label="Name" value={userData.user.name} InputProps={{ readOnly: true }} fullWidth />
@@ -140,14 +148,30 @@ const ReservationPage = ({ selectedDates }) => {
                                 />
                             </Grid>
                         </Grid>
+                        <Grid container justifyContent="center" style={{ width: '100%' }}>
+                            <Button variant="contained" onClick={confirmReservation} sx={{ mt: 1, mb: 2, bgcolor: '#302253', marginTop: '20px', '&:hover': { bgcolor: '#5e2b96' } }}>
+                                Confirm Reservation
+                            </Button>
+                        </Grid>
+
+                        {/* <Button variant="contained" color="primary" style={{ marginTop: '20px' }}>Confirmar Reserva</Button> */}
+
                     </form>
                 </div>
-                
-                <Box display="flex" flexDirection={isSmallScreen ? 'column' : 'row'} alignItems="center">
+
+                {/* <Box display="flex" flexDirection={isSmallScreen ? 'column' : 'row'} alignItems="center">
                     <Divider orientation="horizontal" flexItem variant='inset' />
                     <ProductPolicies />
                     <Divider orientation="horizontal" flexItem variant='inset' />
+                </Box> */}
+
+                <Box component={Paper} elevation={3} p={3} mt={4} >
+                    <Typography variant="h6" style={{ marginBottom: '20px', borderBottom: '2px solid #302253' }}>
+                        Product Policies
+                    </Typography>
+                    <ProductPolicies />
                 </Box>
+
             </Grid>
         </Container>
     );
