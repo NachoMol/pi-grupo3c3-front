12b69b800f6dd4a8d75b44cae9ec73a092cb89b4
luismunozse <luismunozse@gmail.com>
2023-12-11 12:33:12 -0300

    updating

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 // build.js
-import { build } from 'vite'
-import config from './vite.config.js'
+const { build } = require('vite')
+const config = require('./vite.config.js')
 
 build(config).then(() => console.log('Build completed!'))
