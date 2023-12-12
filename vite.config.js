87360188a74e0238cf00f9e9d5da431a0280d757
Luis Muñoz <luismunozse@gmail.com>
2023-12-11 23:36:29 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import emotionBabelPlugin from '@emotion/babel-plugin';
-
+ 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@emotion/react', '@emotion/styled', '@mui/material/Tooltip']
-  },
-  esbuildOptions: {
-    jsxInject: `import React from 'react'`,
-    jsxFactory: `React.createElement`,
-    jsxFragment: `React.Fragment`,
-    plugins: [
-      emotionBabelPlugin  
-    ],
-  },
 })
