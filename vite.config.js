e7990998ec8d6bb6e8ff4a2515d1af589f416df2
Luis Muñoz <luismunozse@gmail.com>
2023-12-11 23:57:15 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
- 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom'], // Ejemplo de cómo dividir las dependencias en un chunk separado
+        },
+      },
+    },
+  },
 })
